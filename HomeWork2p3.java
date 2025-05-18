4166a4a233081ceb6424153486f55621c0f23ea7
BASHAR <162828084+bashar1552@users.noreply.github.com>
2025-05-18 15:07:56 +0300

    Update HomeWork2p3.java

--- HomeWork2p3.java
+++ HomeWork2p3.java
@@ -6,14 +6,14 @@ import java.util.Scanner;
 public class HomeWork2p3 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int yesCount = 0; // عداد لإجابات yes
+        int yesCount = 0; 
 
         for (int i = 1; i <= 20; i++) {
             System.out.print("Question #" + i + " (yes/no): ");
-            String response = input.nextLine().trim(); // نقرأ الإجابة
+            String response = input.nextLine();
 
             if (response.equalsIgnoreCase("yes")) {
-                yesCount++; // نزيد العداد إذا كانت الإجابة yes
+                yesCount++; 
             }
         }
 
